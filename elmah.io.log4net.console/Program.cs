bac41c1bcdc141e1151338fa92189a5b7eae90ce
Thomas Ardal <thomasardal@gmail.com>
2015-05-14 14:06:54 +0200

    Implemented log4net appender for elmah.io

---
+++ elmah.io.log4net.console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4netassembly = log4net;
+
+namespace elmah.io.log4net.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4netassembly.Config.XmlConfigurator.Configure();
+            var log = log4netassembly.LogManager.GetLogger(typeof(Program));
+            log.Debug("This is a debug message");
+            log.Error("This is an error message", new Exception());
+            log.Fatal("This is a fatal message");
+            log.Info("This is an information message");
+            log.Warn("This is a warning message");
+            Console.ReadLine();
+        }
+    }
+}
