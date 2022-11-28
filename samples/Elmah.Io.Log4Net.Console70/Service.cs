49c0d421dbf509fc3263ef2aedde909e1ba6626b
Thomas Ardal <thomasardal@gmail.com>
2022-11-28 10:13:28 +0100

    Added new samples for ASP.NET Core 7.0 and console application on .NET 7

---
+++ samples/Elmah.Io.Log4Net.Console70/Service.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Elmah.Io.Log4Net.Console70
+{
+    public class Service
+    {
+        private readonly ILogger<Service> logger;
+
+        public Service(ILogger<Service> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Execute()
+        {
+            try
+            {
+                var i = 0;
+                var result = 42 / i;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Error during Execute");
+            }
+        }
+    }
+}
