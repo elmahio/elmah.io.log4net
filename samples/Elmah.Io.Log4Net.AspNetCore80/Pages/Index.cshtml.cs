2d02560070088fe8f37d4c222b34575da0a1d5f7
Thomas Ardal <thomasardal@gmail.com>
2023-11-17 14:01:10 +0100

    .NET 8 sample

---
+++ samples/Elmah.Io.Log4Net.AspNetCore80/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Elmah.Io.Log4Net.AspNetCore80.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogWarning("Request to frontpage");
+        }
+    }
+}
